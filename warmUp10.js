6bd8b804753a34c8280c567bb989c3301a470c29
Amera Ayad <amera.ayad.it@gmail.com>
2019-10-29 10:04:01 +0100

    warm up 10

--- warmUp10.js
+++ warmUp10.js
@@ -5,3 +5,5 @@
 // Using jQuery, create a function, that takes the input from the text field, creates a list item and add it to the unordered list, then add a class to the added item alternatively (So first time Class1 then next time Class2 then Class1 and repeat)
 
 // Good Luck!
+
+// the sollution in warmup10sollution folder
