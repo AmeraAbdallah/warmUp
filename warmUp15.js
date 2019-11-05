25e837ed62fe1affe79e09de4e1520e4f4b94089
Amera Ayad <amera.ayad.it@gmail.com>
2019-11-05 09:29:06 +0100

    warmup 15

--- warmUp15.js
+++ warmUp15.js
@@ -4,3 +4,26 @@
 //
 //
 // Note : you can`t use any built-in functions .
+
+function reverseStr(string){
+	string += ' ';
+	var words = [];
+	var wordCounter = 0;
+	var word= '';
+	var result= '';
+	for (var i = 0; i < string.length; i++) {
+		if(string[i] === ' '){
+			words[wordCounter] = word;
+			word = '';
+			++wordCounter;
+		} else {
+			word +=string[i];
+		}
+	}
+	var j = words.length - 1;
+	while(j >= 0){
+		result+= words[j] + ' ';
+		--j;
+	}
+	return result;
+}
