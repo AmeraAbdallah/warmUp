da4581423530a0ac59c9eb04cf1607596919bb62
Amera Ayad <amera.ayad.it@gmail.com>
2019-11-06 09:35:38 +0100

    done warmup 16

--- warmUp16.js
+++ warmUp16.js
@@ -1,5 +1,7 @@
-// Given a string text, you want to use the characters of text to form as many instances of the word "balloon" as possible.
-// You can use each character in text at most once. Return the maximum number of instances that can be formed.
+// Given a string text, you want to use the characters of text to form as many 
+//instances of the word "balloon" as possible.
+// You can use each character in text at most once. Return the maximum number of 
+//instances that can be formed.
 
 // Example 1:
 // Input: text = "nlaebolko"
@@ -16,3 +18,38 @@
 // Constraints:
 // 1 <= text.length <= 10^4
 // text consists of lower case English letters only.
+
+function howManyBalloon(string){
+	var balloon = [0, 0, 0, 0, 0];
+				 //0b 1a 2l 3o 4n
+	if(string.length < 1){
+		return 'enter a string.';
+	}
+
+	if(string.length > 10000){
+		return 'string length is very long, the function accepts only strings that <= 10000';
+	}
+
+	for(var i = 0; i < string.length; i++){
+		if(string[i] === 'b'){
+			balloon[0] = balloon[0] + 1;
+		}
+		else if(string[i] === 'a'){
+			balloon[1] = balloon[1] + 1;
+		}
+		else if(string[i] === 'l'){
+			balloon[2] = balloon[2] + 1;
+		}
+		else if(string[i] === 'o'){
+			balloon[3] = balloon[3] + 1;
+		}
+		else if(string[i] === 'n'){
+			balloon[4] = balloon[4] + 1;
+		}
+	}
+
+	balloon[2] = parseInt(balloon[2] / 2);
+	balloon[3] = parseInt(balloon[3] / 2);
+
+	return Math.min(...balloon);
+}
