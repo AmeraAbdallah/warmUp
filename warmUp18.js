ac5e835c1198662b74f6dc19956520087fbd25e2
Amera Ayad <amera.ayad.it@gmail.com>
2019-11-09 09:23:46 +0100

    done warmup 18

--- warmUp18.js
+++ warmUp18.js
@@ -2,7 +2,7 @@
 
 // For each symbol in the string if it's the first character occurence, replace it with a '1', else replace it with the amount of times you've already seen it...
 
-// But will your code be performant enough?
+// But will your code be performant enough? hmmm
 // Examples:
 
 // input   =  "Hello, World!"
@@ -10,3 +10,17 @@
 
 // input   =  "aaaaaaaaaaaa"
 // result  =  "123456789101112"
+
+function occurences(string){
+	let result = '';
+	for(let i = 0; i < string.length; i++){
+		let acc = 1;
+		for(let j = 0; j < i; j++){
+			if(string[i] === string[j]){
+				++acc;
+			}
+		}
+		result = result + acc;
+	}
+	return result;
+}
